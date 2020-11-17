157aa1035ceb90757619673d037e90b2e34e55dc
yeunjin1 <48876807+yeunjin1@users.noreply.github.com>
2020-11-17 16:45:19 +0900

    Initial commit

---
+++ app/src/main/java/konkuk/yeonj/paymanager/ui/calendar/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package konkuk.yeonj.paymanager.ui.calendar
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/konkuk/yeonj/paymanager/ui/setting/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package konkuk.yeonj.paymanager.ui.setting
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
